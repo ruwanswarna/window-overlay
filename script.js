a4e1fc311ec4819455ca14e2d634d6ace66b02fc
ruwanswarna <ruwanswarna@gmail.com>
2025-11-04 06:37:41 +0530

    eventlistners added for click events and refactored

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 "use strict";
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector("close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
+const closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+const openModal = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-	console.log(btnsOpenModal[i].textContent);
+	btnsOpenModal[i].addEventListener(
+		"click",
+		openModal
+		// function () {
+		// console.log("Button " + (i + 1) + " clicled");
+		// modal.classList.remove("hidden");
+		// overlay.classList.remove("hidden");
+
+		//modal.style.display = "none";
+		//}
+	);
+
+	btnCloseModal.addEventListener(
+		"click",
+		closeModal
+		//     function () {
+		// 	// modal.classList.add("hidden");
+		// 	// overlay.classList.add("hidden");
+		// }
+	);
 }
+
+overlay.addEventListener(
+	"click",
+	closeModal
+	//     function () {
+	// 	modal.classList.add("hidden");
+	// 	overlay.classList.add("hidden");
+	// }
+);
